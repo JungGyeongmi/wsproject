ae4b7751d60beafae5177eaecf6d5b8fad5b326a
JungGyeongmi <gm950715@gmail.com>
2021-12-25 21:52:06 +0900

    20211225_2차

--- VueEx/vuedongsan/src/App.vue
+++ VueEx/vuedongsan/src/App.vue
@@ -2,36 +2,74 @@
   <div id="app">
     <div class="menu">
       <a href="" v-for="(item, idx) in menu" :key="idx">{{item}}</a>
+    </div>  
+    
+    <!-- 모달창 -->
+    <div class="black-bg" v-if="modal">
+      <div class="white-bg">
+        <h4>상세 페이지</h4>
+        <p>상세 페이지 내용</p>
+        <input type="button" 
+          value="닫기" 
+          class="modal-btn"
+          @click="moodal=modal?modal=false:modal=true"
+        >
+      </div>
     </div>
-       
+
     <!-- 원래 이거 따로 받아야하는거 아닌가 return하면 -->
-    <div v-for="(item, idx) in products" :key="item">
-      <h4>{{item}}</h4>
-      <p>{{price[idx]}} 만원</p>
-      <button @click="increase">허위매물신고</button>
-      <span :id="idx">신고수 : {{ count[idx] }}</span>
+    <div>
+      <img src="./assets/room0.jpg" class="room-img">
+      <h4
+      @click="moodal=modal?modal=false:modal=true"
+      >{{products[0]}}</h4>
+      <p>{{price[0]}} 만원</p>
+      <button @click="count[0]++">허위매물신고</button>
+      <span>신고수 : {{ count[0] }}</span>
     </div>
+    <div>
+      <img src="./assets/room1.jpg" class="room-img">
+      <h4>{{products[1]}}</h4>
+      <p>{{price[1]}} 만원</p> 
+      <button @click="count[1] += 2">허위매물신고</button>
+      <span>신고수 : {{ count[1] }}</span>
+    </div>
+    <div>
+      <img src="./assets/room2.jpg" class="room-img">
+      <h4>{{products[2]}}</h4>
+      <p>{{price[2]}} 만원</p>
+      <button @click="count[2]++">허위매물신고</button>
+      <span>신고수 : {{ count[2] }}</span>
+    </div>
+
   </div>
 </template>
 
 <script>
+
+// 경로는 무조건 ./로 시작한다고 생각
+import {apple, apple2} from './oneroom.js'
+    // 작명
+
 export default {
   name: 'App',
   data () {
     return {
+      test: [apple, apple2],
+      modal: false,
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
       menu: ['Home', 'Shop', 'About'],
       price: [50, 100, 70],
-      count: [0, 0, 0]
+      count: [1, 0, 3]
     }
   },
   components: {
   },
   methods: {
-    increase : function (e){
-      let id = e.target.nextElementSibling.id;
+    increase : function (){
+      //let id = e.target.nextElementSibling.id;
       //console.log(this.count[id]++)
-      console.log(id);
+      //console.log(id);
     }
   }
 }
@@ -43,10 +81,11 @@ export default {
 // 2 데이터에 따라 UI가 어떻게 보일지 작성
 
 <style>
+/* 모달창 디자인 */
 body{
   margin: 0;
-  padding: 0;
 }
+
 div{
   box-sizing: border-box;
 }
@@ -63,6 +102,19 @@ div{
   border-radius:8px;
   padding:20px;
 }
+.modal-btn{
+  border-radius: 8px;
+  padding:5px 20;
+  background: skyblue;
+  color: white;
+  font-weight: bold;
+  border: none;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
 
 
 #app {
@@ -71,8 +123,8 @@ div{
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 .h4 {
   color: blue;
 }
---
+++ VueEx/vuedongsan/src/oneroom.js
@@ -0,0 +1,5 @@
+var apple = 10;
+var apple2 = 100;
+
+export default {apple, apple2};
+// export를 해야 내보낼 수 있음
