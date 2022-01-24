d796021d9b63fc71f8c4b76e1ca8184184d662fe
JungGyeongmi <gm950715@gmail.com>
2022-01-25 02:25:45 +0900

    20220125(02:25)-baekjoon

--- 백준_gm/javastudy/function/Ex1065.java
+++ 백준_gm/javastudy/function/Ex1065.java
@@ -1,5 +1,36 @@
-package javastudy.function;
+    package javastudy.function;
 
-public class Ex1065 {
-    
-}
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+
+    public class Ex1065 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            
+            int count = 0;
+            
+            if(N >= 100) {
+                // collections intstream으로 바꿔보기
+                for(int i = 100; i <= N; i++) {
+                    String[] numbers = Integer.toString(i).split("");
+                    int a = Integer.parseInt(numbers[0]);
+                    int b = Integer.parseInt(numbers[1]);
+                    int c = Integer.parseInt(numbers[2]);
+                    
+                    boolean isHan = (a-b==b-c? true : false);
+                    if(isHan) {
+                        System.out.printf("%d %d %d %d%n",a, b, c ,i);
+                        count += 1;
+                    }
+                }
+                count += 99; 
+                // 2자리 수 이상이면 그 이전의 한수 값들을 더해줌 100부터 시작하니까
+            } else {
+                count = N;
+            }
+            System.out.println(count);
+            br.close();
+        }
+    }
--- 백준_gm/javastudy/function/Ex4673.java
+++ 백준_gm/javastudy/function/Ex4673.java
@@ -1,5 +1,35 @@
 package javastudy.function;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Ex4673 {
-    
+    public static void main(String[] args)  {
+        int counter = 0;
+        for(int i = 1; i <= 100; i ++){
+
+            d(i, counter);
+        }
+    }
+    public static void d (int number, int counter) {
+        // origin
+        int origin = number;
+
+
+        String[] tempArr = Integer.toString(number).split("");
+        int[] arr = new int [tempArr.length];
+        for(int i=0; i<tempArr.length; i++) {
+            arr[i] = Integer.parseInt(tempArr[i]);
+        }
+        
+        for(int num : arr) {
+            number += num;
+        }
+
+        if(origin != number) {
+            counter++;
+        }
+        d(number, counter);
+    }
 }
