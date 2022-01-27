e9ea40cff013d96f2943afcdbbb7a2a863cc63a0
JungGyeongmi <gm950715@gmail.com>
2022-01-27 20:52:12 +0900

    20220126

---
+++ 백준_gm/javastudy/stringEx/Ex.java
@@ -0,0 +1,14 @@
+class Ex {
+    import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+    public class Ex11654 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int temp = Integer.parseInt(br.readLine());
+
+            System.out.println(result);
+        }
+}
