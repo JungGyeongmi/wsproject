0469a68669c3cf06bd555062d770ed7c34a45122
JungGyeongmi <gm950715@gmail.com>
2022-02-17 00:16:23 +0900

    20220217
    폴더이동

---
+++ 백준_gm/baekjoon/src/stringEx/Ex11654.java
@@ -0,0 +1,21 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.nio.charset.StandardCharsets;
+
+public class Ex11654 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		char myChar = br.readLine().charAt(0);
+//		System.out.println(myChar);
+		int result = (int)myChar;
+		System.out.println(result);
+		
+//		byte[] bytes = br.readLine().getBytes(StandardCharsets.US_ASCII);
+//		System.out.println(bytes[0]);
+	}
+}
---
+++ 백준_gm/baekjoon/src/stringEx/Ex11720.java
@@ -0,0 +1,24 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex11720 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N =Integer.parseInt(br.readLine());
+		String[] arr = new String[N];
+		arr = br.readLine().split("");
+		int result=0;
+		
+		for(String tem : arr) {
+			result  += Integer.parseInt(tem);
+		}
+		System.out.println(result);
+		
+		br.close();
+	}
+}
